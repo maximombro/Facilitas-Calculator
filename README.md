4390f3132728d8af982f56003e342dcb76f308ae
maximombro <maximombro@gmail.com>
2020-07-17 17:54:30 -0400

    Styled the equations part

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Facilitas Calculator
 A calculator designed to facilitate easy usage as a companion to writing math for, primarily, code applications built on Electron for cross compatibility.
+
+## Uses
+* Node.JS npm
+* Electron
+* Google Fonts
+    * Source Code Pro
--- main.js
+++ main.js
@@ -12,8 +12,8 @@ const { app, BrowserWindow } = require('electron');
 function createWindow() {
     // Create the window
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 600,
+        height: 800,
         webPreferences: {
             nodeIntegration: true
         }
