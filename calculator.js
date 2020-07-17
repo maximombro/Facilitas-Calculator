9fe18cfbef0707f65a60ce3e7f97cffd1cbf296c
maximombro <maximombro@gmail.com>
2020-07-17 19:42:00 -0400

    Added todo comments

--- calculator.js
+++ calculator.js
@@ -4,4 +4,29 @@
     Handles operations of the calculator.
 */
 
-// TODO: Functionality!
+/// Imports
+
+/// Variables
+
+/// Functions
+
+/// Setup Function
+// Function that sets up the calculator
+function setup() {
+    // TODO: Populate the sections and add click events to open their specific menus (trigonometry, function, special, etc)
+
+    // TODO: Populate the sections' specific menus and add click events to add to the current input
+
+    // TODO: Add click events to the numpad grid buttons to add to the current input
+
+    // TODO: Create new equation input from template HTML
+
+    // TODO: Begin watching the input to calculate per change
+
+    // TODO: Add listeners to Enter Grid Button and Enter Key to finish current input
+        // TODO: Move old input up to a stagnant display (with a click listener to copy result?)
+        // TODO: Create new input (as above)
+}
+
+// Setup the calculator
+setup();
